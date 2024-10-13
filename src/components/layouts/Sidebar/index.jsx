6b5e9839affc1fb4b4de4dcb6f2bf924156a30af
hau pham <hau20112003z@gmail.com>
2024-10-13 13:52:21 +0700

    note

--- src/components/layouts/Sidebar/index.jsx
+++ src/components/layouts/Sidebar/index.jsx
@@ -2,10 +2,24 @@ import classNames from "classnames/bind"
 import { Link } from "react-router-dom"
 import styles from './Navbar.module.scss'
 import { iconKhamPha, iconRadio, iconThuVien, iconZingChart } from "~assets/icon"
+import { playIcon } from "~/icon"
+import { useRef, useState } from "react"
+import Zingchart from "~pages/Zingchart"
 
 const c = classNames.bind(styles)
 
 function Navbar(){
+
+    const [click, setClick] = useState(null)
+
+    const handleClick = (link) => {
+        setClick(link)
+    }
+
+    const linkStyle = (link) => {
+        return click === link ? 'focus' : ''
+    }
+
     return (
         <aside className={c('navbar-wrapper')}>
             <div className={c('navbar-logo')}>
@@ -13,8 +27,8 @@ function Navbar(){
             </div>
             <div className={c('navbar-main')}>
                 <ul>
-                    <li className={c('navbar-item')}>
-                        <Link to={'/thuvien'}>
+                    <li onClick={() => handleClick('thuvien')} className={c('navbar-item', linkStyle('thuvien'))}>
+                        <Link title="thư viện" to={'/thuvien'}>
                             <span>
                                 {iconThuVien}
                                 <p>Thư Viện</p>
@@ -22,8 +36,8 @@ function Navbar(){
                         </Link>
                     </li>
 
-                    <li className={c('navbar-item')}>
-                        <Link to={'/'}>
+                    <li onClick={() => handleClick('home')} className={c('navbar-item', linkStyle('home'))}>
+                        <Link title="home" to={'/'}>
                             <span>
                                 {iconKhamPha}
                                 <p>Trang chủ</p>
@@ -31,24 +45,27 @@ function Navbar(){
                         </Link>
                     </li>
                     
-                    <li className={c('navbar-item')}>
-                        <Link to={'/zingchart'}>
+                    <li onClick={() => handleClick('zingchart')} className={c('navbar-item', linkStyle('zingchart'))}>
+                        <Link title="zingchart" to={'/zingchart'}>
                             <span>
                                 {iconZingChart}
                                 <p>Zing chart</p>
+                                <i>{playIcon}</i>
                             </span>
                         </Link>
                     </li>
                     
-                    <li className={c('navbar-item')}>
-                        <Link to={'/radio'}>
+                    <li onClick={() => handleClick('radio')} className={c('navbar-item', linkStyle('radio'))}>
+                        <Link title="radio" to={'/radio'}>
                             <span>
                                 {iconRadio}
                                 <p>Radio</p>
+                                <i>{playIcon}</i>
                             </span>
                         </Link>
                     </li>
                 </ul>
+                <span className={c('line')} style={{margin: '0 20px', backgroundColor: '#858282'}}></span>
             </div>
         </aside>
     )
---
+++ src/icon/index.jsx
@@ -0,0 +1,5 @@
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+export const playIcon = <FontAwesomeIcon icon={faCirclePlay} />
+
