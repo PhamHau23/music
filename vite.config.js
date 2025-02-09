e5f8687c8154f15f6a63517668ce580a00952ba3
PhamHau23 <hau20112003z@gmail.com>
2025-02-09 08:04:28 +0000

    Edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { ViteAliases } from 'vite-aliases'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
