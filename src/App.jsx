5e1757c59fb17fb81fcabfd7d56e7e7c582fcdf0
hau pham <hau20112003z@gmail.com>
2025-02-16 19:20:52 +0700

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { publicRoutes } from './routes'
-import MainLayout from './components/layouts/MainLayout'
+import MainLayout from './components/MainLayout'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
